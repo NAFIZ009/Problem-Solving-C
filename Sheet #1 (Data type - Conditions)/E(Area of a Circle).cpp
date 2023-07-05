2317b8d33fc34fe403f9d332c0120c668614d960
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-07-05 16:56:17 +0600

    s1_e

---
+++ Sheet #1 (Data type - Conditions)/E(Area of a Circle).cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double pie=3.141592653,area,r;
+    cin >> r;
+    area=pie*r*r;
+    cout<<setprecision(9) <<area <<endl;
+    return 0;
+}
