46798959165fbe0c82ea37eb474660764e73af13
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-07-05 17:03:20 +0600

    s1_f

---
+++ Sheet #1 (Data type - Conditions)/F(Digits Summation).cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num1,num2;
+    cin >> num1 >> num2;
+    int sum=(num1%10)+(num2%10);
+    cout<<sum<<endl;
+    return 0;
+}
