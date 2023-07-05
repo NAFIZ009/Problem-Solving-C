be9776470e2b185cfc17c04ae670707118979969
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-07-05 12:46:14 +0600

    s

---
+++ Sheet #1 (Data type - Conditions)/B(Basic Data Types).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double d;
+    cin >> i >> ll >> ch >> f >>d;
+    cout << i << "\n" << ll << "\n" << ch << "\n" << f << "\n" <<d;
+
+    return 0;
+}
